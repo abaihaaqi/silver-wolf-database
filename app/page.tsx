d1db00ea7d1f9c8bc1e9b88d642a2d47fd91b094
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2023-09-22 17:16:10 +0700

    Add website disclaimer ✍️

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import Card from "@/components/Card";
 import CurrentBanner from "@/components/CurrentBanner";
 
 export default function Home() {
@@ -6,6 +7,22 @@ export default function Home() {
     <div className="bg-[url('/stars.png')]">
       <Banner />
       <CurrentBanner />
+      <div className="px-2 pb-2">
+        <Card className="p-2">
+          <h1 className="text-base font-bold text-center">Disclaimer</h1>
+          This website is created by{" "}
+          <a
+            href="http://nizarbaihaqi.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            Nizar Baihaqi
+          </a>{" "}
+          only for example project purposes only, any game assets are property
+          of COGNOSPHERE PTE. LTD.
+        </Card>
+      </div>
     </div>
   );
 }
